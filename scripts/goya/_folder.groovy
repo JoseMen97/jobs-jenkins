5ffc888d1755aa7047bd91e21819ad6d95672bd8
JoseMen97 <69913439+JoseMen97@users.noreply.github.com>
2023-06-20 21:33:06 -0500

    Update _folder.groovy

--- scripts/goya/_folder.groovy
+++ scripts/goya/_folder.groovy
@@ -1,3 +1,10 @@
+import org.jenkinsci.plugins.scriptsecurity.scripts.ScriptApproval
+
+ScriptApproval scriptApproval = ScriptApproval.get()
+scriptApproval.pendingScripts.each {
+    scriptApproval.approveScript(it.hash)
+}
+
 folder('proyect_liquibase') {
     displayName('proyect_liquibase2')
     description('Folder for project Liquibase')
